38075e50c24dbd202dce65ce07a7b6e2fe79cabe
HoshinoHitomi <hiikochan711@gmail.com>
2021-06-07 20:16:34 +0000

    [Update]booksコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'users/show'
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
