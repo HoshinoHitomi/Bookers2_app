8fc7ca54bf218c7cc24b344d162d97f8360c24c0
HoshinoHitomi <hiikochan711@gmail.com>
2021-06-12 07:07:42 +0000

    [Add]showページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'users/show'
-  get 'homes/about'
   resources :books, only: [:index, :new, :create, :show, :destroy]
+  get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
