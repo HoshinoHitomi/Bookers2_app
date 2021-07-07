5d73e3510ca088c0c25671b9d1d33f750396d6ed
HoshinoHitomi <hiikochan711@gmail.com>
2021-07-07 10:45:48 +0000

    [Finish]カラムを追加して再提出

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_083531) do
+ActiveRecord::Schema.define(version: 2021_07_07_100314) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "profile_images", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
